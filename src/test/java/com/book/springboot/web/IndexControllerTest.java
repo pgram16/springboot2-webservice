c0d5b5bdb4922ed74b1bf2c27a59a63830c163c4
pigram <pgram16@naver.com>
2022-03-16 18:05:45 +0900

    조회/수정/삭제 기능 추가

--- src/test/java/com/book/springboot/web/IndexControllerTest.java
+++ src/test/java/com/book/springboot/web/IndexControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -17,6 +18,11 @@ public class IndexControllerTest {
 
     @Test
     public void 메인페이지_로딩(){
+        //when
+        String body = this.restTemplate.getForObject("/",String.class);
+
+        //then
+        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
 
     }
 }
